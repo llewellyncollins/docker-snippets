19b540a898fad72e608c4d60c2a71939e7699bd7
Llewellyn <llewellyn.collins@live5gaming.com>
2019-07-21 22:42:54 +0100

    Use SNippet Item in Snippet List

--- client/src/components/SnippetList.vue
+++ client/src/components/SnippetList.vue
@@ -1,44 +1,25 @@
 <template>
     <div>
         <div v-for="(snippet, index) in snippets" :key="index">
-            <v-card :to="`/snippet/${snippet.id}`" class="mb-3" hover ripple>
-                <v-card-title primary-title>
-                    <div>
-                        <h3 class="snippet-name headline mb-0">{{ snippet.name }}</h3>
-                        <div>
-                            By User:
-                            <span class="snippet-author">
-                                {{
-                                snippet.author
-                                }}
-                            </span>
-                        </div>
-                    </div>
-                    <v-spacer></v-spacer>
-                    <Counter label="downlads" :value="snippet.downloads" />
-                    <Counter label="stars" :value="snippet.stars" />
-                </v-card-title>
-
-                <v-card-text class="snippet-description">
-                    {{
-                    snippet.description
-                    }}
-                </v-card-text>
-
-                <v-card-actions>
-                    <v-btn flat color="orange">Star</v-btn>
-                    <v-btn flat color="orange">Customise</v-btn>
-                </v-card-actions>
-            </v-card>
+            <SnippetItem
+                :id="snippet.id"
+                :name="snippet.name"
+                :author="snippet.author"
+                :description="snippet.description"
+                :content="snippet.content"
+                :downloads="snippet.downloads"
+                :stars="snippet.stars"
+            />
         </div>
     </div>
 </template>
 <script>
-import Counter from "@/components/Counter";
+import SnippetItem from "@/components/SnippetItem";
+
 export default {
     name: "SnippetList",
     components: {
-        Counter
+        SnippetItem
     },
     props: {
         snippets: Array
